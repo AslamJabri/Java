f559bea8b17981b39c84792dc4674a9c4244273f
Aslam Jabri <jabri.aslam@gmail.com>
2022-03-05 22:29:45 +0530

    First java project

---
+++ Project1/src/Tree.java
@@ -0,0 +1,13 @@
+public class Tree{
+    public static void main(String[] args){
+        //we will introduce ourself and print a tree
+        System.out.println("Hey There, I'm Trying to learn Java!");
+        System.out.println("I am going to plant a tree today.");
+        System.out.println(" * ");
+        System.out.println("***");
+        System.out.println("***");
+        System.out.println(" * ");
+        System.out.println(" * ");
+        System.out.println("~~~~");
+    }
+}
