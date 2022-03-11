3380f9937bf4c8adde94425ff4968c7490eaf058
Aslam Jabri <jabri.aslam@gmail.com>
2022-03-11 16:40:47 +0530

    primitive datatypes

--- variables/challenge1.java
+++ variables/challenge1.java
@@ -5,5 +5,13 @@ public class challenge1 {
         int myintNumber = 120015;
         long myLongNumber = 500000L  +10L *(myByteNumber + myshortNumber+ myintNumber);
         System.out.println("The long Number: "+ myLongNumber);
+
+
+        //challenge2
+
+        double pound = 200d;
+        double  kg = 0.45359237d;
+        double total = pound * kg;
+        System.out.println("Total kilograms: "+total);
     }
 }
--- variables/primitive.java
+++ variables/primitive.java
@@ -21,6 +21,27 @@ public class primitive {
         long myLongMaxValue = Long.MAX_VALUE;
         System.out.println("Min Long Value : " + myLongMinValue);
         System.out.println("Max Long Value: " + myLongMaxValue);
+
+        float myfloatMinValue = Float.MIN_VALUE;
+        float myfloatMaxValue = Float.MAX_VALUE;
+        System.out.println("Min float Value : " + myfloatMinValue);
+        System.out.println("Max float Value: " + myfloatMaxValue);
+
+        double mydoubleMinValue = Double.MIN_VALUE;
+        double mydoubleMaxValue = Double.MAX_VALUE;
+        System.out.println("Min Double Value : " + mydoubleMinValue);
+        System.out.println("Max Double Value: " + mydoubleMaxValue);
+
+        int intValue = 5/3;
+        float fValue = 5f/3f;//casting and can also be used 5.25f
+        double dValue = 5d/3d;
+        System.out.println("Int Value: "+ intValue +"\n" + "fvalue: " + fValue +"\n"+ "dvalue: "+ dValue);
+        
+        char myChar = 'D';
+        char myUnicode = '\u00A9';
+        System.out.println("Char: "+ myChar );
+        System.out.println("Unicode: "+ myUnicode);
+
     }
 }
 
