22d71a9f5f220da72bc9fae0b00ae8a1e016b647
Aslam Jabri <jabri.aslam@gmail.com>
2022-03-11 16:03:16 +0530

    primitive challenge1

--- variables/challenge1.java
+++ variables/challenge1.java
@@ -3,7 +3,7 @@ public class challenge1 {
         byte myByteNumber = 125;
         short myshortNumber = 1025;
         int myintNumber = 120015;
-        long myLongNumber = 50000 + (myByteNumber*10) + myshortNumber+ myintNumber;
+        long myLongNumber = 500000L  +10L *(myByteNumber + myshortNumber+ myintNumber);
         System.out.println("The long Number: "+ myLongNumber);
     }
 }
