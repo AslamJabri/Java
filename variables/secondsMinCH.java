aa9298e1ab5f092426e0102ff95db6980e33a1ca
Aslam Jabri <jabri.aslam@gmail.com>
2022-03-17 00:15:51 +0530

    SecMinch

--- variables/secondsMinCH.java
+++ variables/secondsMinCH.java
@@ -1,16 +1,23 @@
 public class secondsMinCH {
+
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
+        
+    
     public static void main(String[] args) {
-        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(65, 5));
         System.out.println(getDurationString(400L));
+        System.out.println(getDurationString(-41));
+        System.out.println(getDurationString(2345, 46));
     }
     
     public static String getDurationString(long min,long second){
         if ((min < 0) || (second < 0) || (second > 59)){
-            return "Invalid Value";
+            return INVALID_VALUE_MESSAGE;
         }
         long hours = min/60;
         long remMin = min % 60;
         String hoursString = hours + "h";
+
         if (hours < 10){
             hoursString = "0" + hoursString;    
         }
@@ -29,7 +36,7 @@ public class secondsMinCH {
     }
     public static String getDurationString( long second){
         if (second < 0){
-            return "Invalid Value";
+            return INVALID_VALUE_MESSAGE;
         }
         long min = second/60;
         long remsec = second % 60;
