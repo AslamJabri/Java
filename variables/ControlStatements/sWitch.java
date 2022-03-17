940a093cc0ff2127a959ff17483a86c634b3e397
Aslam Jabri <jabri.aslam@gmail.com>
2022-03-17 22:45:04 +0530

    controlflow

--- variables/ControlStatements/sWitch.java
+++ variables/ControlStatements/sWitch.java
@@ -2,7 +2,7 @@ package ControlStatements;
 
 public class sWitch {
     public static void main(String[] args) {
-        char newChar = 'A';
+        char newChar = 'D';
         switch(newChar){
             case 'A':
                 System.out.println("This is A");
@@ -10,16 +10,10 @@ public class sWitch {
             case 'B':
                 System.out.println("This is B");
                 break;
-            case 'C':
-                System.out.println("This is C");
+            case 'C':case 'D': case 'E':
+                System.out.println("This is C , D ,E" + " This is the char value " +newChar + ". ");
                 break;
-            case 'D':
-                System.out.println("This is D");
-                break;
-                //using a break statement helps in breaking the flow when the condition is meet.
-            case 'E':
-                System.out.println("This is E");
-                break;
+            
             default:
                 System.out.println("It is neither A, B , C,D,E");
                 break;
