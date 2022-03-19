2658c3d85ce5300a92aa21a3dbebd8010e36f44b
Aslam Jabri <jabri.aslam@gmail.com>
2022-03-19 15:01:21 +0530

    forloop

---
+++ variables/ControlStatements/forLoop.java
@@ -0,0 +1,25 @@
+package ControlStatements;
+
+public class forLoop {
+    public static void main(String[] args) {
+        for (double i = 2; i < 9; i++){
+            System.out.println("10000 at " + i +"% intrest = " + String.format("%.2f",calculateIntrest(10000.0, i)));
+        }
+        System.out.println("************");
+        for (double i = 8; i > 1; i--){
+            System.out.println("10000 at " + i +"% intrest = " + String.format("%.2f",calculateIntrest(10000.0, i)));
+           
+        }
+        
+        for (int i = 0; i <= 100 ; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+          
+        }
+    }
+    public static double calculateIntrest(double amount , double intrest){
+        return (amount *( intrest/100));
+    }
+    
+}
